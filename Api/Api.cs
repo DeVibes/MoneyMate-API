905704eb4464d0a19864ed188fde89f28995082a
Mark Poliatskin <mark.poliatskin@vaicamedical.com>
2022-09-15 23:12:13 +0300

    refactor: remove test code

--- Api/Api.cs
+++ Api/Api.cs
@@ -6,19 +6,11 @@ public static class Api
     {
         // Map /transactions endpoints
         app.MapGet("/", () => "App is running!");
-        app.MapGet("/TestEnvVar", TestEnvVars);
         app.MapGet("/transactions", TransactionsAPI.GetAllTransactions);
         app.MapGet("/transactions/{id}", TransactionsAPI. GetTransaction);
         app.MapPost("/transactions", TransactionsAPI.InsertTransaction);
         app.MapDelete("/transactions/{id}", TransactionsAPI.DeleteTransaction);
         app.MapPut("/transactions/{id}", TransactionsAPI.UpdateTransaction);
         app.MapMethods("/transactions/{id}", new[] { "PATCH" }, TransactionsAPI.PatchTransaction);
-    }
-
-    public static IResult TestEnvVars(IConfiguration configuration) 
-    {
-        var env = configuration["TestVar"] ?? "Not found";
-        var env2 = configuration["TestVar:Sub"] ?? "Not found sub";
-        return Results.Ok($"{env} - {env2}");
     }
 }
